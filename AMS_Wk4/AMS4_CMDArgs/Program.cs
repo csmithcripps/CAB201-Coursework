e78deecbe733cc7f75c32399353abe221005f5d1
csmithcripps <codysmithcripps@outlook.com>
2020-08-23 23:23:05 +1000

    AMS4 Complete

---
+++ AMS_Wk4/AMS4_CMDArgs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+
+class CommandLineArguments
+{
+    public static void Main(string[] args)
+    {   
+        if (args.Length == 0)
+        {
+            Console.WriteLine("WARNING: No command line arguments provided.");
+        }
+        else if (args[0][0] != '-')
+        {
+            Console.WriteLine("WARNING: Parameters must be provided after options.");
+        }
+        else
+        {
+            for (int argumentNum = 0; argumentNum < args.Length; argumentNum++)
+            {
+                if (args[argumentNum][0] == '-')
+                {
+                    Console.Write(args[argumentNum]);
+
+                    int paramPointer = argumentNum + 1;
+
+                    if (paramPointer == args.Length)
+                    {
+                        paramPointer = argumentNum;
+                    }
+
+                    while (args[paramPointer][0] != '-')
+                    {
+                        if (paramPointer == argumentNum + 1)
+                        {
+                            Console.Write(": { ");
+                        }
+                        else
+                        {
+                            Console.Write(", ");
+                        }
+                        Console.Write(args[paramPointer]);
+
+                        paramPointer++;
+                        if (paramPointer >= args.Length) break;
+                    }
+                    if (paramPointer > argumentNum + 1)
+                    {
+                        Console.Write(" }");
+                    }
+                    Console.Write("\n");
+                }            
+            }
+        }
+    }
+}
---
+++ AMS_Wk4/AMS4_Sequence/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Sequence
+{
+
+    public static string[] Kmers(int substringLength, params string[] sequence)
+    {
+        string fullSequence = string.Join("", sequence);
+        string[] mers = new string[(fullSequence.Length - substringLength+1)];
+
+        for(int i = 0; i<(fullSequence.Length - substringLength + 1); i++)
+        {
+            mers[i] = fullSequence.Substring(i, substringLength);
+        }
+
+        return mers;
+    }
+
+    public static void ReverseComplement(ref string sequence)
+    {
+        string complement = "";
+        for(int i = (sequence.Length-1); i>=0; i--)
+        {
+            switch (sequence[i])
+            {
+                case 'A':
+                    complement += 'T';
+                    break;
+
+                case 'T':
+                    complement += 'A';
+                    break;
+
+                case 'C':
+                    complement += 'G';
+                    break;
+
+                case 'G':
+                    complement += 'C';
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        sequence = complement;
+    }
+
+    public static bool Dyad(string sequence, int dyadMin = 5)
+    {
+        string seq1 = sequence.Substring(0, dyadMin);
+        ReverseComplement(ref seq1);
+
+        string seq2 = sequence.Substring(sequence.Length - dyadMin);
+
+        if (seq1 == seq2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Kmers(4, "AGAGTG", "TCGAT").Length);
+
+        string sequence = "ACTGA";
+        ReverseComplement(ref sequence);
+
+        Console.WriteLine(sequence);
+
+        Console.WriteLine(Dyad("ACTGA...TCAGT"));
+    }
+}
